1acdfb8b1dbd68f2039d42950c426582907a2516
ggross523 <ggross0@chicagobooth.edu>
2025-05-12 15:11:18 +0000

    last part

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,26 +1,26 @@
 class BoardsController < ApplicationController
   def index
     matching_boards = Board.all
-
     @list_of_boards = matching_boards.order({ :created_at => :desc })
-
     render({ :template => "boards/index" })
   end
-
+  
   def show
     the_id = params.fetch("path_id")
-
     matching_boards = Board.where({ :id => the_id })
-
     @the_board = matching_boards.at(0)
-
-    render({ :template => "boards/show" })
+    
+    # Add this check to handle the case when the board doesn't exist
+    if @the_board.nil?
+      redirect_to("/boards", { :alert => "Board not found." })
+    else
+      render({ :template => "boards/show" })
+    end
   end
-
+  
   def create
     the_board = Board.new
     the_board.name = params.fetch("query_name")
-
     if the_board.valid?
       the_board.save
       redirect_to("/boards", { :notice => "Board created successfully." })
@@ -28,27 +28,35 @@ class BoardsController < ApplicationController
       redirect_to("/boards", { :alert => the_board.errors.full_messages.to_sentence })
     end
   end
-
+  
   def update
     the_id = params.fetch("path_id")
     the_board = Board.where({ :id => the_id }).at(0)
-
-    the_board.name = params.fetch("query_name")
-
-    if the_board.valid?
-      the_board.save
-      redirect_to("/boards/#{the_board.id}", { :notice => "Board updated successfully."} )
+    
+    # Add this check to handle the case when the board doesn't exist
+    if the_board.nil?
+      redirect_to("/boards", { :alert => "Board not found." })
     else
-      redirect_to("/boards/#{the_board.id}", { :alert => the_board.errors.full_messages.to_sentence })
+      the_board.name = params.fetch("query_name")
+      if the_board.valid?
+        the_board.save
+        redirect_to("/boards/#{the_board.id}", { :notice => "Board updated successfully."} )
+      else
+        redirect_to("/boards/#{the_board.id}", { :alert => the_board.errors.full_messages.to_sentence })
+      end
     end
   end
-
+  
   def destroy
     the_id = params.fetch("path_id")
     the_board = Board.where({ :id => the_id }).at(0)
-
-    the_board.destroy
-
-    redirect_to("/boards", { :notice => "Board deleted successfully."} )
+    
+    # Add this check to handle the case when the board doesn't exist
+    if the_board.nil?
+      redirect_to("/boards", { :alert => "Board not found." })
+    else
+      the_board.destroy
+      redirect_to("/boards", { :notice => "Board deleted successfully."} )
+    end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,60 +1,67 @@
 class PostsController < ApplicationController
   def index
     matching_posts = Post.all
-
     @list_of_posts = matching_posts.order({ :created_at => :desc })
-
     render({ :template => "posts/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
-    matching_posts = Post.where({ :id => the_id })
-
-    @the_post = matching_posts.at(0)
-
-    render({ :template => "posts/show" })
+    @the_post = Post.find_by(id: the_id)
+    
+    if @the_post.nil?
+      redirect_to("/posts", { :alert => "Post not found." })
+    else
+      render({ :template => "posts/show" })
+    end
   end
 
   def create
     the_post = Post.new
-    the_post.title = params.fetch("query_title")
-    the_post.body = params.fetch("query_body")
-    the_post.expires_on = params.fetch("query_expires_on")
-    the_post.board_id = params.fetch("query_board_id")
-
+    the_post.title = params.fetch("title")
+    the_post.body = params.fetch("body")
+    the_post.expires_on = params.fetch("expires_on")
+    the_post.board_id = params.fetch("board_id")
+    
     if the_post.valid?
       the_post.save
-      redirect_to("/posts", { :notice => "Post created successfully." })
+      redirect_to("/boards/#{the_post.board_id}", { :notice => "Post created successfully." })
     else
-      redirect_to("/posts", { :alert => the_post.errors.full_messages.to_sentence })
+      redirect_to("/boards/#{the_post.board_id}", { :alert => the_post.errors.full_messages.to_sentence })
     end
   end
 
   def update
     the_id = params.fetch("path_id")
-    the_post = Post.where({ :id => the_id }).at(0)
-
-    the_post.title = params.fetch("query_title")
-    the_post.body = params.fetch("query_body")
-    the_post.expires_on = params.fetch("query_expires_on")
-    the_post.board_id = params.fetch("query_board_id")
-
-    if the_post.valid?
-      the_post.save
-      redirect_to("/posts/#{the_post.id}", { :notice => "Post updated successfully."} )
+    the_post = Post.find_by(id: the_id)
+    
+    if the_post.nil?
+      redirect_to("/posts", { :alert => "Post not found." })
     else
-      redirect_to("/posts/#{the_post.id}", { :alert => the_post.errors.full_messages.to_sentence })
+      the_post.title = params.fetch("query_title")
+      the_post.body = params.fetch("query_body")
+      the_post.expires_on = params.fetch("query_expires_on")
+      the_post.board_id = params.fetch("query_board_id")
+      
+      if the_post.valid?
+        the_post.save
+        redirect_to("/posts/#{the_post.id}", { :notice => "Post updated successfully."} )
+      else
+        redirect_to("/posts/#{the_post.id}", { :alert => the_post.errors.full_messages.to_sentence })
+      end
     end
   end
 
   def destroy
     the_id = params.fetch("path_id")
-    the_post = Post.where({ :id => the_id }).at(0)
-
-    the_post.destroy
-
-    redirect_to("/posts", { :notice => "Post deleted successfully."} )
+    the_post = Post.find_by(id: the_id)
+    
+    if the_post.nil?
+      redirect_to("/posts", { :alert => "Post not found." })
+    else
+      board_id = the_post.board_id
+      the_post.destroy
+      redirect_to("/boards/#{board_id}", { :notice => "Post deleted successfully."} )
+    end
   end
 end
